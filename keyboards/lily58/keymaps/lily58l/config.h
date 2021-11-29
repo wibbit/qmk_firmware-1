f62ce6c0902b646046948f0cbc6705adc505a7eb
Douglas Furlong <douglas.furlong@gmail.com>
2021-11-29 09:17:22 +0000

    Update for my specific boards
    Required I believe to get my EliteC v4's to flash correctly

--- keyboards/lily58/keymaps/lily58l/config.h
+++ keyboards/lily58/keymaps/lily58l/config.h
@@ -35,5 +35,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-// #define SPLIT_USB_DETECT
+#define SPLIT_USB_DETECT
 // #define NO_USB_STARTUP_CHECK
